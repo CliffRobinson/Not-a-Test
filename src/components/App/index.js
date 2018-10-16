87163d65fbf19f612a15a3c06845bc1f0db13ec2
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-10-17 12:35:37 +1300

    initial commit, .gitignore(!) lets see if manual changes show up in the sandbox

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,7 @@ export default function App({ handleOnSubmit, latestUser, users }) {
     <div className="App">
       {/* add welcome message */}
       <p>Add your name below to register!</p>
+      <p>Some arbitrary text </p>
       <form onSubmit={handleOnSubmit}>
         <input type="text" name="user" />
         <input type="submit" value="Register user" />
