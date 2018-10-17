66faa5afca7293dd1aaaf63a569945eab93c56ea
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-10-17 13:44:40 +1300

    where is redux state?

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,15 @@
-import { mapIndexed } from 'ramda-adjunct'
+import { mapIndexed, isUndefined } from 'ramda-adjunct'
+
 import React from 'react'
 
-export default function App({ handleOnSubmit, latestUser, users }) {
+export default function App({ handleOnSubmit, latestUser, users, nextUserId }) {
+  
   return (
     <div className="App">
-      {/* add welcome message */}
-      <p>Add your name below to register!</p>
-      <p>Some arbitrary text </p>
+      {isUndefined(latestUser) 
+        ? <p> Welcome to the app! No users have registered yet. </p>
+        : <p> Welcome to the app! The latest user to register is ${latestUser.name}. </p>}
+        Next user id will be {nextUserId}
       <form onSubmit={handleOnSubmit}>
         <input type="text" name="user" />
         <input type="submit" value="Register user" />
